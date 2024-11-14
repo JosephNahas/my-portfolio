6a05244a37ea04ab1c44569ad863e5567a796098
Joseph Nahas <joe_nahas@hotmail.com>
2024-11-14 14:42:56 -0500

    Added Form validation

--- js/script.js
+++ js/script.js
@@ -27,3 +27,29 @@ for (const button of detailButtons){
         }
     });
 } 
+
+let contactName = document.getElementById('name');
+let contactEmail = document.getElementById('email');
+let contactMessage = document.getElementById('message');
+let submitButton = document.getElementById('submit-btn');
+
+submitButton.addEventListener("click", function(event){
+    event.preventDefault();
+    if (contactName.value === '')
+        alert("Please enter your name");
+    else if (contactEmail.value === '')
+        alert("Please enter your email");
+    else if (contactMessage.value === '')
+        alert("Please enter your message");
+    else if (!(contactEmail.value.toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )))
+        alert("please enter a valid email")
+    else{
+        alert("Thank you for your message!")
+        contactName.value = '';
+        contactEmail.value = '';
+        contactMessage.value = '';
+    }
+});
