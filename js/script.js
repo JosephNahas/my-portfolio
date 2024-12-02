e790e97628f2f13318a1402664e5a751d14b9215
Joseph Nahas <joe_nahas@hotmail.com>
2024-12-02 17:41:40 -0500

    Implemented project filtering

--- js/script.js
+++ js/script.js
@@ -68,3 +68,19 @@ toggleThemeButton.addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
     aboutSection.classList.toggle('dark-mode');
 });
+
+const filterButtons = document.querySelectorAll('.filter-btn');
+const projects = document.querySelectorAll('.projects-box');
+
+filterButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        const category = this.getAttribute('data-category');
+        projects.forEach(project => {
+            if (project.getAttribute('data-category') === category || category === 'all') {
+                project.style.display = 'block';
+            } else {
+                project.style.display = 'none';
+            }
+        });
+    });
+});
